3bf04aa1e7ccf03f41c38f81fe7859cfe5ec0f10
Franco Balich <franco.balich@gmail.com>
2023-07-06 23:46:56 -0300

    Add cors config

---
+++ src/middleware/corsConfig.js
@@ -0,0 +1,7 @@
+export const corsConfig = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT')
+  res.header('Allow', 'GET, POST, PUT')
+  next()
+}
