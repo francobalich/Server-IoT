7349166060c14afa77bd104a4f1574c30bdd7f24
Franco Balich <franco.balich@gmail.com>
2023-07-07 00:09:38 -0300

    Add basic mqtt

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import { corsConfig } from './middleware/corsConfig.js'
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import * as mqtt from 'mqtt'
 import { testRouter } from './routes/test.routes.js'
 dotenv.config()
 const PORT = process.env.PORT || 3000
+// Mqtt
+const protocol = 'mqtt'
+const host = 'broker.emqx.io'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 
+const connectUrl = `${protocol}://${host}:${port}`
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
+
+client.on('connect', () => {
+  console.log('Connected')
+})
 // Configuración del server
 const app = express()
 app.use(express.json())
@@ -14,6 +34,8 @@ app.use(express.json())
 // Configuración del cors
 app.use(cors())
 app.use(corsConfig)
+
+
 
 // Rutas
 app.use('/api/', testRouter)
